c18b56077e56432e4b004542d95db3161b3005bc
FengYNing <fengyning@gmail.com>
2012-04-15 21:32:27 +0800

    Initial Commit

---
+++ little_pig/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  little_pig
+//
+//  Created by  on 12-4-15.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
